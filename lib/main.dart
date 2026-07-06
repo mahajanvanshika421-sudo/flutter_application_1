a3fda728d02446c2dc5d869b9457c75b7dfa3881
mahajanvanshika421-sudo <mahajanvanshika421@gmail.com>
2026-07-06 10:08:04 +0530

    list view builder task

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/listbuild.dart';
+import 'package:flutter_application_1/practice.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ListBuilder()
+      home: ListBuild()
     );
   }
 }
--- lib/practice.dart
+++ lib/practice.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ListBuild extends StatefulWidget {
   const ListBuild({super.key});
@@ -9,7 +11,14 @@ class ListBuild extends StatefulWidget {
 
 class _ListBuildState extends State<ListBuild> {
 
-List<String> nameList = [];
+List<String> nameList = [
+  "Rahul",
+  "Vanshu",
+  "Sunny",
+
+];
+TextEditingController
+nameController=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,12 +38,145 @@ List<String> nameList = [];
                       padding:EdgeInsets.all(10),
                       child: Text(nameList[index]), 
                       ),
-                  ],)
-              )
+                      Row(
+                        children: [
+                          ElevatedButton(
+                            onPressed:(){ 
+                            nameController.text=nameList[index];
+                            updateDialog(context,index);
+                            },
+                            child: Text("Update"),
+                            ),
+                            ElevatedButton(
+                              onPressed:(){
+                                setState(){
+                                  nameList.removeAt(index);
+                                }
+                              },
+                              child:Text("Delete"),
+                              ),
+                        ],
+                      ),
+                  ],
+        
+        ),
+              );
             }
-          )
+          ),
+          Positioned(
+            bottom: 10,
+right: 10,
+         child:FloatingActionButton(
+          onPressed:(){
+            nameController.clear();
+            addDialog(context);
+
+          } ,
+          child:Icon(Icons.add),
+
+          ),
+          ),
         ],
       )
     );
   }
+  void addDialog(BuildContext context){
+    showDialog(
+      context: context,
+      builder: (context){
+        return Dialog(
+          child:Padding(
+            padding:EdgeInsets.all(10) ,
+            child:Column(
+              mainAxisSize:MainAxisSize. min,
+              children: [
+                Text(
+                  "Add Name",
+                style:TextStyle(fontSize:20),
+            ),
+            SizedBox(height:10),
+            TextField(
+              controller:nameController,
+              decoration:InputDecoration(
+                hintText:"Enter Your Name",
+                border:OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height:10),
+            ElevatedButton(
+              onPressed:(){
+                if(nameController.text.isEmpty){
+                  Fluttertoast.showToast(
+                    msg:"Enter Your Name", 
+                    
+                    );
+                }else{
+                  setState(){
+                    nameList.add(nameController.text);
+                    nameController.clear();
+                  }
+                  Navigator.pop(context);
+                }
+              } ,
+              child:Text("Save Name"),
+              ),
+              ],
+            ),
+            ),
+        );
+      },
+    );
+  }
+  void updateDialog(BuildContext context,int index){
+    showDialog(
+      context:context,
+      builder:(context){
+        return Dialog(
+          child:Padding(
+            padding:EdgeInsets.all(10),
+            child:Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "UpdATE Name",
+                style:TextStyle(fontSize:20),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                hintText: "eNTER yOUR Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed:(){
+                if(nameController.text.isEmpty){
+                  Fluttertoast.showToast(
+                    msg:"eNTER yOUR Name", 
+                    );
+
+                }else{
+                  setState(() {
+                    nameList[index]=nameController.text;
+                    nameController.clear();
+               
+               
+                                    });
+                                    Navigator.pop(context);
+                }
+              } ,
+              child:Text("Update Name"),
+              ),
+            
+
+            ],
+            ),
+            ),
+        );
+      },
+    );       
+  }
 }
+
